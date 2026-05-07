e7ea9b353fd6b9e0ea4afb32a73b14cde9c141ae
hcongminh052-lang <hcongminh052@gmail.com>
2026-05-07 22:09:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,7 @@ import signal
 import traceback
 import discord
 from discord.ext import commands
-
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is running!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
+from keep_alive import keep_alive
 
 prefix = "!"
 intents = discord.Intents.all()
